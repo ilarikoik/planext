6d82f36ca906c819980fb83f827719688e0f72c1
ilarikoik <ilari.koik@gmail.com>
2025-04-06 16:13:07 +0300

    trip mobile font

--- src/components/trips.tsx
+++ src/components/trips.tsx
@@ -19,13 +19,13 @@ export default function TripList({ list }: triplist) {
                 className="h-32 w-full max-w-[700px] flex-row justify-center items-center m-5 p-10"
               >
                 <div className="flex flex-col justify-center items-center hover:cursor-pointer">
-                <h2 className="font-semibold text-3xl text-accent">
-                  {item.place}
-                </h2>
-                <hr className="border-2 border-accent w-4/5" />
-                <h2 className="font-semibold text-3xl text-accent">
-                  {item.year}
-                </h2>
+                  <h2 className="text-xl font-semibold md:text-3xl text-accent">
+                    {item.place}
+                  </h2>
+                  <hr className="border-2 border-accent w-4/5" />
+                  <h2 className="text-lg font-semibold md:text-2xl text-accent">
+                    {item.year}
+                  </h2>
                 </div>
               </div>
             </>
