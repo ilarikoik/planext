733c1b26632f3a343e31925ee4f6b7094b9a5fc3
ilarikoik <ilari.koik@gmail.com>
2025-04-07 18:32:41 +0300

    bug

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -1,14 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import config from "../config";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5AoVywNFFQV8Y93c8jXXEB1ecU7Ft8f8",
-  authDomain: "travelist-28548.firebaseapp.com",
-  projectId: "travelist-28548",
-  storageBucket: "travelist-28548.firebasestorage.app",
-  messagingSenderId: "1035761810303",
-  appId: "1:1035761810303:web:79ac3a81eade265023b84a",
+  apiKey: config.REACT_APP_API_KEY,
+  authDomain: config.REACT_APP_AUTH_DOMAIN,
+  projectId: config.REACT_APP_PROJECT_ID,
+  storageBucket: config.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: config.REACT_APP_MESSAGING_SENDER_ID,
+  appId: config.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
